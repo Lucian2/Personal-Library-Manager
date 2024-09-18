84abcb3942250f3cb7d7cfe176d5916817a49c42
lucian-popp <popplucian@yahoo.com>
2024-09-18 15:47:18 +0300

    initial-project-setup

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1f6bff',
+    },
+    secondary: {
+      main: '#37ca57',
+    },
+  },
+});
+
+export default theme;
